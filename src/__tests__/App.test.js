772d75b3c0a0716cbaffd538ea83f55485ccf7f2
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-11-05 11:01:22 +0000

    feat: App renders Shoot component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../components/App';
+import Shoot from '../components/Shoot';
 
 describe("App", () => {
-  it("renders without crashing", () => {
-    shallow(<App />);
+  it("will render Shoot", () => {
+    const wrapper = shallow(<App />);
+    const shoot = <Shoot />
+    expect(wrapper.contains(shoot)).toEqual(true)
   });
 });
---
+++ src/components/Shoot.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Shoot extends Component {
+  render() {
+    return (
+      <h3> This is the Shoot component </h3>
+    )
+  }
+}
+
+export default Shoot;
