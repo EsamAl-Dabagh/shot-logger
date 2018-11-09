16eaa946659dae40905ed0f1d43379c80dea255e
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-11-09 11:20:59 +0000

    feat: each newly created object has unique id

--- src/components/Shoot.js
+++ src/components/Shoot.js
@@ -12,7 +12,11 @@ class Shoot extends Component {
   addTake = () => {
     const takes = [...this.state.takes];
 
-    takes.push({ id: 1 });
+    const ids = this.state.takes.map(take => take.id);
+
+    const max_id = ids.length === 0 ? 0 : Math.max(...ids);
+
+    takes.push({ id: max_id + 1 });
 
     this.setState({ takes })
   }
