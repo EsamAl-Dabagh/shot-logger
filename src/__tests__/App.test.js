91e69e8f0d927b259c9dd1f6df7a13297319cf4f
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-11-09 10:10:33 +0000

    refactor: to use snapshot

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,9 +4,8 @@ import App from '../components/App';
 import Shoot from '../components/Shoot';
 
 describe("App", () => {
-  it("will render Shoot", () => {
-    const wrapper = shallow(<App />);
-    const shoot = <Shoot />
-    expect(wrapper.contains(shoot)).toEqual(true)
+  it("will render correctly", () => {
+    const app = shallow(<App />);
+    expect(app).toMatchSnapshot();
   });
 });
