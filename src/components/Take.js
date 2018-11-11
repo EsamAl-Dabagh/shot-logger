524844fb1511afa5ee695774ae9063208c12e889
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-11-11 10:17:48 +0000

    refactor: remove import of FormController from Material UI

--- src/components/Take.js
+++ src/components/Take.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 class Take extends Component {
   constructor() {
